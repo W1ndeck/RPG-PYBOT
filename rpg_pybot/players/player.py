6399b8c883030a5dd089e8c42dd2ae6071c5ce6d
Lucas Mateus <lmateus1067@outlook.com>
2022-10-11 09:27:18 -0300

    .

--- rpg_pybot/players/player.py
+++ rpg_pybot/players/player.py
@@ -1,6 +1,7 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, classe=None, breed=None):
         self._name = str(name)
+        self._classe = "classless"
         self._hp = 100
         self._mp = 100
         self._level = 0
@@ -15,8 +16,10 @@ class Player:
         self.name = new_name
 
     def __str__(self):
-        return f"NAME: {self._name}\n\tLEVEL: {self._level}\n\tEXP: {self._xp}\n\tHP: {self._hp}\n\tMP: {self._mp},    "
+        return f"NAME: {self._name}\n\tLEVEL: {self._level}\n\tCLASS: {self._classe}\n\tEXP: {self._xp}\n\tHP: {self._hp}\n\tMP: {self._mp}"
+
+    def player_data(self):
+        return print(f"NAME: {self._name}\n\tLEVEL: {self._level}\n\tCLASS: {self._classe}\n\tEXP: {self._xp}\n\tHP: {self._hp}\n\tMP: {self._mp}")
 
 
-jogador = Player("Lucas")
-print(jogador)
+jogador = Player("Lucas", 12)
