81704c4aa46a586ce1531e2b6691cdc730e5af1d
Lucas Mateus <lmateus1067@outlook.com>
2022-10-11 09:48:17 -0300

    add classes

--- rpg_pybot/players/player.py
+++ rpg_pybot/players/player.py
@@ -1,5 +1,5 @@
 class Player:
-    def __init__(self, name, classe=None, breed=None):
+    def __init__(self, name):
         self._name = str(name)
         self._classe = "classless"
         self._hp = 100
@@ -16,10 +16,38 @@ class Player:
         self.name = new_name
 
     def __str__(self):
-        return f"NAME: {self._name}\n\tLEVEL: {self._level}\n\tCLASS: {self._classe}\n\tEXP: {self._xp}\n\tHP: {self._hp}\n\tMP: {self._mp}"
+        return f"NAME: {self._name}\n\tLEVEL: {self._level}\n\tCLASS: {self._classe}\n\tEXP: {self._xp}\n\tHP: {self._hp}\n\tMP: {self._mp}\n"
 
     def player_data(self):
-        return print(f"NAME: {self._name}\n\tLEVEL: {self._level}\n\tCLASS: {self._classe}\n\tEXP: {self._xp}\n\tHP: {self._hp}\n\tMP: {self._mp}")
+        return print(f"NAME: {self._name}\n\tLEVEL: {self._level}\n\tCLASS: {self._classe}\n\tEXP: {self._xp}\n\tHP: {self._hp}\n\tMP: {self._mp}\n")
+
+    def set_char_class(self, class_number):
+
+        try:
+            if int(class_number) >= 0 or int(class_number) <= 4:
+                if int(class_number) == 0:
+                    self._classe = "Bard"
+                    self._hp = 80
+                    self._mp = 150
+                elif int(class_number) == 1:
+                    self._classe = "Barbarian"
+                    self._hp = 200
+                    self._mp = 60
+                elif int(class_number) == 2:
+                    self._classe = "Rogue"
+                    self._hp = 50
+                    self._mp = 50
+                elif int(class_number) == 3:
+                    self._classe = "Warrior"
+                    self._hp = 120
+                    self._mp = 50
+                elif int(class_number) == 4:
+                    self._classe = "Mage"
+                    self._hp = 80
+                    self._mp = 200
+
+        except:
+            print("Invalid command")
 
 
-jogador = Player("Lucas", 12)
+jogador = Player()
